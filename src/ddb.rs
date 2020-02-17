7d38d41e0f6d578a5b8e47a421688ed77e8f1461
Javier Arrieta <javier.arrieta@zalando.ie>
2020-02-17 20:20:10 +0000

    Use dynamodb repo

--- src/ddb.rs
+++ src/ddb.rs
@@ -1,36 +1,48 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use futures::future::{BoxFuture, Future, FutureExt};
-use rusoto_core::RusotoError;
-use rusoto_dynamodb::{AttributeValue, DynamoDb, GetItemError, GetItemInput, GetItemOutput};
+use futures::future::{BoxFuture, FutureExt};
+use rusoto_dynamodb::{AttributeValue, DynamoDb, GetItemInput, GetItemOutput};
 
 use crate::models::Customer;
 use crate::repos::CustomerRepository;
-use std::pin::Pin;
 
+#[derive(Clone)]
 pub struct CustomerDDBClient {
-    client: Arc<dyn DynamoDb + Send + Sync>,
+    pub client: Arc<dyn DynamoDb + Send + Sync>,
+    pub table_name: String,
 }
 
 impl CustomerRepository for CustomerDDBClient {
     fn get_customer(&self, id: String) -> BoxFuture<'static, Option<Customer>>  {
-        let request = GetItemInput::default();
+        let mut request = GetItemInput::default();
+
+        let mut key_attr_value = AttributeValue::default();
+        key_attr_value.n = Some(id);
+        let mut key: HashMap<String, AttributeValue> = HashMap::new();
+        key.insert("customer_id".to_owned(), key_attr_value);
+
+        request.table_name = self.table_name.clone();
+        request.key = key;
+
         let client = self.client.clone();
 
         async move {
             let item = client.get_item(request).await.ok()?;
-            get_item_to_customer(item)
+            get_item_to_customer(&item)
         }.boxed()
     }
 }
 
 #[inline]
-fn get_item_to_customer(get_item: GetItemOutput) -> Option<Customer> {
-    get_item.item.and_then(parse_attributes_to_customer)
+fn get_item_to_customer(get_item: &GetItemOutput) -> Option<Customer> {
+    get_item.item.as_ref().and_then( |m| parse_attributes_to_customer(&m))
 }
 
 #[inline]
-fn parse_attributes_to_customer(m: HashMap<String, AttributeValue>) -> Option<Customer> {
-    unimplemented!()
+fn parse_attributes_to_customer(m: &HashMap<String, AttributeValue>) -> Option<Customer> {
+    let id = m.get("customer_id").and_then(|v| v.n.clone())?;
+    let name = m.get("name").and_then(|v| v.n.clone())?;
+
+    Some(Customer::new(id, name))
 }
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ mod repos;
 mod handlers;
 mod ddb;
 
-use std::collections::HashMap;
-
 use warp::Filter;
-use models::Customer;
-use repos::DummyCustomerRepository;
 use handlers::{handle, handle_recover};
+use rusoto_dynamodb::DynamoDbClient;
+use rusoto_core::Region;
+use crate::ddb::CustomerDDBClient;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
 
-    let customer_db: HashMap<String, Customer> = vec![Customer::new_from_str("1", "Joe Bloggs"), Customer::new_from_str("2", "John Doe"), Customer::new_from_str("3", "Peter Parker")]
-        .into_iter().map(|c| (c.id.clone(), c)).collect();
+//    let customer_db: HashMap<String, Customer> = vec![Customer::new_from_str("1", "Joe Bloggs"), Customer::new_from_str("2", "John Doe"), Customer::new_from_str("3", "Peter Parker")]
+//        .into_iter().map(|c| (c.id.clone(), c)).collect();
 
-    let customer_repo = DummyCustomerRepository::new(customer_db);
+    let ddb_client: DynamoDbClient = DynamoDbClient::new(Region::EuCentral1);
+
+    let customer_repo = CustomerDDBClient { client: Arc::new(ddb_client), table_name: "javi_customer".to_owned()  };
 
     let hello = warp::path!("customers" / String)
         .and(warp::get())
